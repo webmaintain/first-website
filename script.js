fcaa11ee10850664590adb2947e3654d52a2ca42
webmaintain <tmlabib8@gmail.com>
2023-10-26 06:31:14 +0600

    project done

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+/*
+
+// Get a reference to the contact menu item
+const contactMenuItem = document.querySelector('.menu-area li a #tmfooter');
+
+// Get a reference to the footer
+const footer = document.getElementById('#footer');
+
+// Function to show the footer when the Contact menu item is clicked
+function showFooter() {
+    footer.style.display = 'block';
+}
+
+// Add a click event listener to the Contact menu item
+contactMenuItem.addEventListener('click', showFooter);
+
+*/
